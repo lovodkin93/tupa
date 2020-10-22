ffcefafd6cca17de23385273a932391b13f295cc
lovodkin93 <lovodkin93@gmail.com>
2020-10-22 13:59:07 +0300

    .

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from tupa.parse import * #from .parse import *
 if __name__ == "__main__":
-    import os
-    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+    #import os
+    #os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
     main()
