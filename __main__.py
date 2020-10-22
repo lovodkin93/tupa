b1552ab39144542b2571401eead0ef88b5077314
lovodkin93 <lovodkin93@gmail.com>
2020-10-22 13:55:45 +0300

    add main

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 from tupa.parse import * #from .parse import *
 if __name__ == "__main__":
-    #print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__, __name__, str(__package__)))
+    import os
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
     main()
